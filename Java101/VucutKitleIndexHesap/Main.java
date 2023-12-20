ee7f9ac52dfff0cc7b98e8d1aba7bd3dc6c1518b
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:46:09 +0300

    Add files via upload

--- Java101/VucutKitleIndexHesap/Main.java
+++ Java101/VucutKitleIndexHesap/Main.java
@@ -1,1 +1,16 @@
+/* Kilo (kg) / (Boy(m) * Boy(m)) */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double kilo,boy,vki;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Kilonuzu giriniz(kg cinsinden) : ");
+        kilo = scan.nextDouble();
+        System.out.println("Boyunuzu Giriniz(metre cinsinden) : ");
+        boy = scan.nextDouble();
+        vki = kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+vki);
 
+    }
+
+}
