ca3e8fe0058bc7969f128699f70c96ae5d4128aa
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:33:24 +0300

    Add files via upload

--- Java101/Girilen4eBolunenSayTop/Main.java
+++ Java101/Girilen4eBolunenSayTop/Main.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n1, toplam=0;
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.print("Sayı Girin: ");
+            n1 = input.nextInt();
+            if(n1%4==0){
+                toplam+=n1;
+            }
+        }while(n1%2==0);
+        System.out.println("Sonuç: " + toplam);
+    }
+}
