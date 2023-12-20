2295f4ec6eb81ef7227cf584fc03034ac659d69c
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:34:22 +0300

    Add files via upload

--- Java101/GirSay4ve5Kuvvetleri/Main.java
+++ Java101/GirSay4ve5Kuvvetleri/Main.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sınır Sayısını Giriniz : ");
+        n = scan.nextInt();
 
+        for (int i = 1; i <= n;i*=4){
+            System.out.println("4'ün "+ count + ". kuvveti: " + i);
+            count++;
+        }
+        count=0;
+        for (int i = 1; i <= n;i*=5){
+            System.out.println("5'in "+ count + ". kuvveti: " + i);
+            count++;
+        }
+    }
+}
