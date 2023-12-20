6b591e50c4b8e6ca9fc8931b0bd45f49dfd2eb70
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:44:17 +0300

    Add files via upload

--- Java101/UcgenAlanHesap/Main.java
+++ Java101/UcgenAlanHesap/Main.java
@@ -1,1 +1,20 @@
+/*Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.*/
+import java.util.Scanner;
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        double alan,u;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("İlk Kenarı Giriniz : ");
+        a = scan.nextInt();
+        System.out.println("İkinci Kenarı Giriniz : ");
+        b = scan.nextInt();
+        System.out.println("Üçüncü Kenarı Giriniz : ");
+        c = scan.nextInt();
 
+        u = (a+b+c)/2;
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Üçgenin Alanı : "+alan);
+    }
+}
