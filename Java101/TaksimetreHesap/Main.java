205864c06b42f2160cc7a9b011c4ad949f623957
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:42:38 +0300

    Add files via upload

--- Java101/TaksimetreHesap/Main.java
+++ Java101/TaksimetreHesap/Main.java
@@ -1,1 +1,24 @@
+/*Taksimetre KM başına 2.20 TL tutmaktadır.
+Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+Taksimetre açılış ücreti 10 TL'dir.
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int baslangıcTutar = 10;
+        double km,tutar;
+        double perKm = 2.20;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Mesafeyi KM Cinsinden Giriniz : ");
+        km = scan.nextDouble();
+        tutar=km*perKm;
+        tutar+=baslangıcTutar;
 
+        if(tutar<20){
+            System.out.println("Ödenecek Tutar 20 TL\'dir.");
+        }
+        else{
+            System.out.println("Ödenecek Tutar "+tutar+" TL\'dir.");
+        }
+    }
+}
