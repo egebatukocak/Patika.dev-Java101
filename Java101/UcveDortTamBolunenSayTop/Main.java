855db96a19da525ee09cadc4a56ce04ae7d68da3
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:45:09 +0300

    Add files via upload

--- Java101/UcveDortTamBolunenSayTop/Main.java
+++ Java101/UcveDortTamBolunenSayTop/Main.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int k,total;
+        total = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayı Giriniz : ");
+        k = scan.nextInt();
 
+        for(int i = 0;i<=k;i++){
+            if(i%3==0 && i%4==0){
+                total+=i;
+            }else{
+                continue;
+            }
+        }
+        System.out.println("3\'e ve 4\'e Tam Bölünen Sayıların Toplamı : "+total);
+        scan.close();
+    }
+}
