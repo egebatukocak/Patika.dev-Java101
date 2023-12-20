69074462dd7f0e3053356eab16c724743b3a9a89
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:36:39 +0300

    Add files via upload

--- Java101/KDVTutarHesap/Main.java
+++ Java101/KDVTutarHesap/Main.java
@@ -1,1 +1,27 @@
+/* KDV 1.8
+*Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+* tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.*/
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        double tutar,kdvTutar,kdvliTutar,kdv;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ücret Tutarını Giriniz : ");
+        tutar = scan.nextDouble();
+        if(tutar>0 && tutar<1000){
+            kdv=0.18;
+            kdvTutar = tutar*kdv;
+            kdvliTutar = tutar + kdvTutar;
+            System.out.println("KDVli Tutar : "+kdvliTutar);
+        }
+        else{
+            kdv=0.08;
+            kdvTutar = tutar*kdv;
+            kdvliTutar = tutar + kdvTutar;
+            System.out.println("KDVli Tutar : "+kdvliTutar);
+        }
+
+    }
+}
