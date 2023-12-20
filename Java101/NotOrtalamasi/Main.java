cb5ae352b3ce9bc5c3050454453c2c37c26d683f
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:39:41 +0300

    Add files via upload

--- Java101/NotOrtalamasi/Main.java
+++ Java101/NotOrtalamasi/Main.java
@@ -1,1 +1,35 @@
+/*Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin
+ sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+ Ve eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int mat,fiz,kim,turk,tar,muz;
+        float ort;
+        Scanner scan= new Scanner(System.in);
 
+        System.out.println("Matematik notu : ");
+        mat = scan.nextInt();
+        System.out.println("Fizik notu : ");
+        fiz = scan.nextInt();
+        System.out.println("Kimya notu : ");
+        kim = scan.nextInt();
+        System.out.println("Türkçe notu : ");
+        turk = scan.nextInt();
+        System.out.println("Tarih notu : ");
+        tar = scan.nextInt();
+        System.out.println("Müzik notu : ");
+        muz = scan.nextInt();
+
+        ort= (float) (mat+fiz+kim+turk+tar+muz)/6;
+
+        System.out.println("Ortalama : "+ort);
+        if(ort>60){
+            System.out.println("Sınıfı Geçti.");
+        }
+        else{
+            System.out.println("Sınıfta Kaldı.");
+        }
+
+    }
+}
