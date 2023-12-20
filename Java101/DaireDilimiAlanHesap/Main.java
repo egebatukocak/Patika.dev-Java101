3d02165a75c55fc8e55430c8e5b8b51a9a6b8630
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 14:32:21 +0300

    Add files via upload

--- Java101/DaireDilimiAlanHesap/Main.java
+++ Java101/DaireDilimiAlanHesap/Main.java
@@ -1,1 +1,21 @@
+/* Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
 
+𝜋 sayısını = 3.14 alınız.
+
+Formül : (𝜋 * (r*r) * 𝛼) / 360*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int r,a;
+        double pi,alan;
+        pi = 3.14;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dairenin Yarı Çapını Giriniz : ");
+        r = scan.nextInt();
+        System.out.println("Daire Diliminin Açısını Giriniz : ");
+        a = scan.nextInt();
+        alan = (pi*r*r*a)/360;
+
+        System.out.println("Daire Diliminin Alanı : "+alan);
+    }
+}
