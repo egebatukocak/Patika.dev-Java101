10650d79b0af7e0b531122f64ba9c5da4323eda1
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 15:29:20 +0300

    Add files via upload

--- Java101/UsluSayiHesap/Main.java
+++ Java101/UsluSayiHesap/Main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        int n,h;
+        int s=1;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Üssü hesaplanacak sayı : ");
+        n = scan.nextInt();
+        System.out.println("Üssü olacak sayı : ");
+        h = scan.nextInt();
+
+        for(int i=1;i<=h;i++){
+            s*=n;
+        }
+        System.out.println(s);
+    }
+}
