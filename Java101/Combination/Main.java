5be4f726a60a7511985ca4c95f4b0d1fa29b82cc
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-20 15:17:18 +0300

    Add files via upload

--- Java101/Combination/Main.java
+++ Java101/Combination/Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        int n,r,fakn,fakr,fakt;
+        double total;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("n değerini giriniz : ");
+        n = scan.nextInt();
+        fakn = 1;
+        fakr = 1;
+        fakt = 1;
+        System.out.println("r değerini giriniz : ");
+        r = scan.nextInt();
+        for(int i = 1;i<=n;i++){
+            fakn *= i;
+        }
+        for(int i = 1;i<=r;i++){
+            fakr *= i;
+        }
+        for(int i = 1;i<=(n-r);i++){
+            fakt *= i;
+        }
+        total = fakn / (fakr*fakt);
+        System.out.println("Kombinasyon sonucu : "+total);
+
+    }
+}
