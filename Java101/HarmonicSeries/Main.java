c740d0236cad85be920829771ba2de5664bb5525
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-21 15:23:06 +0300

    Add files via upload

--- Java101/HarmonicSeries/Main.java
+++ Java101/HarmonicSeries/Main.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        int number;
+        double result = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayı Giriniz : ");
+        number = scan.nextInt();
+
+        for(int i=1;i<=number;i++){
+            result += (double) 1 /i;
+        }
+        System.out.println("Harmonik Seri : "+result);
+        scan.close();
+    }
+}
