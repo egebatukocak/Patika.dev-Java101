7d2db4186b9a8c6cbc3a5209225aa1fecec14062
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-22 12:03:50 +0300

    Add files via upload

--- Java101/MaxMinHesap/Main.java
+++ Java101/MaxMinHesap/Main.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int count;
+        int n;
+        int minn = Integer.MAX_VALUE;
+        int maxn = Integer.MIN_VALUE;
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz : ");
+        count = scan.nextInt();
+        for(int i=1;i<=count;i++){
+            System.out.println(i+". Sayıyı giriniz: ");
+            n = scan.nextInt();
+
+            if (n > maxn) {
+                maxn = n;
+            }
+
+            if (n < minn) {
+                minn = n;
+            }
+        }
+        System.out.println("En büyük sayı: "+maxn);
+        System.out.println("En küçük sayı: "+minn);
+        scan.close();
+    }
+}
