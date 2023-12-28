481f5be80bc1775f37ae716325c75498e37f381d
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-28 13:35:55 +0300

    Update TersUcgenCizme.java

--- Java101/TersUcgenCizme/TersUcgenCizme.java
+++ Java101/TersUcgenCizme/TersUcgenCizme.java
@@ -1,1 +1,23 @@
-
+import java.util.Scanner;
+public class TersUcgenCizme {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Basamak sayısnı giriniz: ");
+            int n = scan.nextInt();
+            int basamak = 2*n-1;
+            for (int i=n; i>0; i--)
+            {
+                for (int j=0; j<(n-i); j++)
+                {
+                    System.out.print(" ");
+                }
+                for (int k = basamak; k > 0; k--)
+                {
+                    System.out.print("*");
+                }
+                basamak-=2;
+                System.out.println();
+            }
+            scan.close();
+        }
+    }
