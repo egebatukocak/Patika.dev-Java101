cd15da7669613f47debbe7c230a48218eb44bfff
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-28 16:59:21 +0300

    Add files via upload

--- Java101/Fibonacci/Fibonacci.java
+++ Java101/Fibonacci/Fibonacci.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n1,n2,n3,s;
+        n1=0;
+        n2=1;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Eleman Sayısı : ");
+        s= scan.nextInt();
+        System.out.println();
+        System.out.print(n1+" "+n2);
+        for(int i=2;i<s;i++){
+            n3 =n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+        scan.close();
+    }
+}
