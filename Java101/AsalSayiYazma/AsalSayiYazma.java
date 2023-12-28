5e7089504e24dcea02267eff74f3a04be06a8dcd
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-28 13:46:48 +0300

    Add files via upload

--- Java101/AsalSayiYazma/AsalSayiYazma.java
+++ Java101/AsalSayiYazma/AsalSayiYazma.java
@@ -1,1 +1,16 @@
-
+public class AsalSayiYazma {
+    public static void main(String[] args) {
+        for(int i=2;i<=100;i++){
+            int control=0;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    control=1;
+                    break;
+                }
+            }
+            if(control==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
