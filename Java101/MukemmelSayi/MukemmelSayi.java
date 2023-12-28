ee119f4b1253214fe3d832b27454eb8d687b0402
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-28 13:23:24 +0300

    Add files via upload

--- Java101/MukemmelSayi/MukemmelSayi.java
+++ Java101/MukemmelSayi/MukemmelSayi.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        int total = 0;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int n = scan.nextInt();
+
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                total+=i;
+            }
+        }
+        if (total == n) {
+            System.out.println(n + " Mükemmel Sayıdır");
+        } else if (n < 0) {
+            System.out.println("Lütfen Pozitif Sayı Giriniz");
+        } else {
+            System.out.println(n + " Mükemmel Sayı Değildir !");
+        }
+        scan.close();
+    }
+}
