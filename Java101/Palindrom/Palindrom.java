7b7513bf8c03d75799823b913f1b7aa8acb3b8eb
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-29 09:25:17 +0300

    Add files via upload

--- Java101/Palindrom/Palindrom.java
+++ Java101/Palindrom/Palindrom.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Palindrom {
+    static boolean isPalindrom(int number){
+        int temp = number, reverseNumber = 0,lastNumber;
+        while(temp!=0){
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10)+ lastNumber;
+            temp/=10;
+        }
+        if(reverseNumber==number)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        int number;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Palindrom Sayı Sorgulama");
+        System.out.println("Bir Sayı Giriniz : ");
+        number = scan.nextInt();
+        System.out.println(isPalindrom(number));
+        scan.close();
+    }
+}
