1513b1ee89a385c95f1d9077ffb3b206142b1fab
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-29 16:51:30 +0300

    Add files via upload

--- Java101/RecursivePower/RecursivePower.java
+++ Java101/RecursivePower/RecursivePower.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class RecursivePower {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Taban değerini giriniz : ");
+        int base = scan.nextInt();
+        System.out.println("Üs değerini giriniz : ");
+        int pow = scan.nextInt();
+        System.out.println("Sonuç : "+getPow(base,pow));
+    }
+    static int getPow(int base,int pow){
+        if(pow==0)
+            return 1;
+        return base*getPow(base,pow-1);
+    }
+}
