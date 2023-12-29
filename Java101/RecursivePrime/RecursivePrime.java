bddf649249591b4f8af78963c303afdf26386d42
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2023-12-29 17:13:04 +0300

    Add files via upload

--- Java101/RecursivePrime/RecursivePrime.java
+++ Java101/RecursivePrime/RecursivePrime.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class RecursivePrime {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayı Giriniz : ");
+        int number = scan.nextInt();
+        if(isPrime(number,number/2) == true)
+            System.out.println(number + " sayısı ASALDIR !");
+        else
+            System.out.println(number + " sayısı ASAL DEĞİLDİR !");
+        scan.close();
+    }
+    static boolean isPrime(int number,int i){
+
+        if(i==1) return true;
+        else {
+            if (number%i==0){
+                return false;
+            }
+            else return isPrime(number,i-1);
+        }
+    }
+}
