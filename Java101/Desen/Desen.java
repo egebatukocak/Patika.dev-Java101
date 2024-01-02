4d8abd1e183418481273921114872c88f4fff264
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2024-01-02 15:43:51 +0300

    Add files via upload

--- Java101/Desen/Desen.java
+++ Java101/Desen/Desen.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Desen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir n değeri girin: ");
+        int n = scanner.nextInt();
+
+        desen(n);
+
+        scanner.close();
+    }
+
+    static void desen(int a){
+        System.out.print(a + " ");
+
+        if (a <= 0)
+        {
+            return;
+        }
+        desen(a - 5);
+        if (a > 0)
+        {
+            System.out.print(a + " ");
+
+        }
+    }
+}
