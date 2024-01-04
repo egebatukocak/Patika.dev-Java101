d123e5c8e41310d1a63c7dfb07f5f3f526bb7584
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2024-01-04 10:56:17 +0300

    Add files via upload

--- Java101/Transpose/Transpose.java
+++ Java101/Transpose/Transpose.java
@@ -1,1 +1,28 @@
+public class Transpose {
+    public static void main(String[] args) {
+        int [][] matrix = { {2,3,4},
+                {5,6,4} };
+        System.out.println("Before the change");
+        printArray(matrix);
+        System.out.println("After the change");
+        change(matrix);
+    }
+    public static void change(int[][] matrix){
+        int [][] transpose = new int[matrix[0].length][matrix.length];
+        for (int rows = 0; rows < matrix.length; rows++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                transpose[col][rows] = matrix[rows][col];
+            }
+        }
+        printArray(transpose);
+    }
 
+    public static void printArray(int[][] matrix) {
+        for(int[] row : matrix){
+            for(int col : row){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+}
