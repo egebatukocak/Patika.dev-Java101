564d51d30e6ae57d0c077eca8d4776692918a635
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2024-01-04 14:18:03 +0300

    Add files via upload

--- Java101/PalindromWord/PalindromWord.java
+++ Java101/PalindromWord/PalindromWord.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class PalindromWord {
+    static boolean isPalindrome(String str){
+        int i = 0;
+        int j = str.length()-1;
+        while(i<j){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sorgulanacak kelimeyi giriniz : ");
+        String word = scan.nextLine();
+        if(isPalindrome(word)){
+            System.out.println(word + " kelimesi palindromik bir kelimedir.");
+        }
+        else{
+            System.out.println(word + " kelimesi palindromik bir kelime değildir.");
+        }
+    }
+}
