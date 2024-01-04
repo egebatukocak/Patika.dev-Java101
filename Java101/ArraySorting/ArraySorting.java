44c847ebfb8910948ba5e007aaa82c8b24b481f9
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2024-01-04 10:35:40 +0300

    Add files via upload

--- Java101/ArraySorting/ArraySorting.java
+++ Java101/ArraySorting/ArraySorting.java
@@ -1,1 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class ArraySorting {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Dizinin Boyutu n : ");
+        int n = scan.nextInt();
+        int[] number = new int[n];
+        System.out.println("Dizinin Elemanlarını Giriniz : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i+1) + ". Elamanı : ");
+            number[i] = scan.nextInt();
+        }
+        System.out.println("Dizi : " + Arrays.toString(number));
+        Arrays.sort(number);
+        System.out.println("Sıralama : " + Arrays.toString(number));
+
+        scan.close();
+    }
+}
