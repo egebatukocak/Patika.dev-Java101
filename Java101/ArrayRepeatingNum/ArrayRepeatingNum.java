60bcdd615c39dd7a172a1643dff1d7f188f76e12
Ege Batuğ KOÇAK <99660189+egebatukocak@users.noreply.github.com>
2024-01-04 10:45:05 +0300

    Add files via upload

--- Java101/ArrayRepeatingNum/ArrayRepeatingNum.java
+++ Java101/ArrayRepeatingNum/ArrayRepeatingNum.java
@@ -1,1 +1,31 @@
+import java.util.Arrays;
+
+public class ArrayRepeatingNum {
+    public static void main(String[] args) {
+        int[] array1 = {10, 20, 20, 10, 10, 20, 5, 20};
+        System.out.println("Dizi : " + Arrays.toString(array1));
+        int[] frequencies = new int[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            if (frequencies[i] == -1) {
+                continue;
+            }
+            int element = array1[i];
+            int frequency = 1;
+            for (int j = i + 1; j < array1.length; j++) {
+                if (array1[j] == element) {
+                    frequency++;
+                    frequencies[j] = -1;
+                }
+            }
+            frequencies[i] = frequency;
+        }
+            System.out.println("Tekrar Sayıları");
+            for (int i = 0; i < array1.length; i++) {
+                if (frequencies[i] != -1) {
+                    System.out.println(array1[i] + " sayısı " + frequencies[i] + " kez tekrarlandı. ");
+                }
+            }
+        }
+    }
 
